f21d853628c00bcd788c3b6299504224d7c9cf06
Dick Karlsson <dick.karlsson@live.se>
2020-08-28 17:10:11 +0200

    MAde version 2.0

--- FizzBuzz/FizzBuzz.cpp
+++ FizzBuzz/FizzBuzz.cpp
@@ -3,9 +3,38 @@
 // for multiples of 5 it should print "Buzz". If it is a multiple of both, the program should print "FizzBuzz".
 
 #include <iostream>
+#include <string>
 
 int main()
 {
+	int UPPER = 100;
+	
+
+	for (int i = 1; i <= UPPER; ++i)
+	{
+		std::string out;
+		
+		out.append(std::to_string(i));
+		out.append(". ");
+		
+		if (i % 3 == 0)
+			out.append("Fizz");
+		
+		if (i % 5 == 0)
+			out.append("Buzz");
+
+		if (out.find("Buzz") != std::string::npos)
+			out.append(" lightyear");
+
+		std::cout << out << "\n";
+
+	}
+}
+
+/*
+int main()
+{
+	// v 1.0
 	int i = 1;
 	
 	do
@@ -31,5 +60,5 @@ int main()
 	}while (i < 25);
     
 }
-
+*/
 
